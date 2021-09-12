1a51597332a71774bbbdafd6f0e43a0a576eed72
gucio321 <gucio321@protonmail.com>
2021-09-12 17:34:42 +0200

    initial commit - add base go module with base cosinus / sinus generator

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gucio321/singen
+
+go 1.16
+
+require github.com/AllenDang/giu v0.5.6 // indirect
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"math"
+	"time"
+
+	"github.com/AllenDang/giu"
+)
+
+var (
+	startx, endx int32 = -10, 10
+	xvalues            = []float64{float64(startx)}
+)
+
+func loop() {
+	sinvalues := []float64{}
+	cosvalues := []float64{}
+	for _, v := range xvalues {
+		sinvalues = append(sinvalues, math.Sin(v))
+		cosvalues = append(cosvalues, math.Cos(v))
+	}
+
+	giu.SingleWindow().Layout(
+		giu.Plot("").AxisLimits(float64(startx), float64(endx), -2, 2, giu.ConditionOnce).Plots(
+			giu.PlotLineXY("sin", xvalues, sinvalues),
+			giu.PlotLineXY("cos", xvalues, cosvalues),
+		),
+		giu.Row(
+			giu.Label("x min: "),
+			giu.InputInt(&startx).OnChange(func() {
+				xvalues = []float64{0}
+			}),
+			giu.Label("x max: "),
+			giu.InputInt(&endx).OnChange(func() {
+				xvalues = []float64{float64(startx)}
+			}),
+		),
+	)
+}
+
+func main() {
+	wnd := giu.NewMasterWindow("generator", 640, 480, 0)
+	go func() {
+		for range time.Tick(100 * time.Millisecond) {
+			xvalues = append(xvalues, xvalues[len(xvalues)-1]+0.03)
+		}
+	}()
+	wnd.Run(loop)
+}
